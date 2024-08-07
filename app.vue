6b420d9d38a8525f023df2af47f1316ef0edcf88
teschiopol <concolatop@gmail.com>
2024-08-07 22:13:26 +0200

    init

---
+++ app.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+  const search = ref('')
+</script>
+
+<template>
+  <div class="min-h-screen">
+    <div class="text-center">
+      <h1 class="mt-0 pt-4 mb-2 text-4xl font-bold text-primary">
+        OnlyMaya - Food 🍕
+      </h1>
+      <p class="font-sans text-lg text-slate-500">
+        List of place where I eat with my friends 🐼
+      </p>
+    </div>
+    <div class="flex justify-content-center mb-4">
+      <IconField>
+        <InputIcon class="pi pi-search" />
+        <InputText v-model="search" placeholder="Search" autocomplete="off" />
+    </IconField>
+    </div>
+  </div>
+</template>
+
+<style>
+html, body {
+  margin: 0;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,32 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primevue/themes/aura';
+
+export default defineNuxtConfig({
+  devtools: { enabled: false },
+  modules: [
+    '@primevue/nuxt-module',
+    '@nuxt/eslint',
+  ],
+  eslint: {
+    config: {
+      stylistic: {
+        semi: true,
+      },
+    },
+  },
+  css: ['primevue/resources/themes/aura-light-pink/theme.css', 'primeicons/primeicons.css', 'primeflex/primeflex.css'],
+  primevue: {
+    options: {
+      ripple: true,
+      theme: {
+        preset: Aura,
+      },
+    },
+    autoImport: true,
+  },
+  app: {
+    head: {
+      title: 'OnlyMaya - Food 🍕',
+    },
+  },
+});
